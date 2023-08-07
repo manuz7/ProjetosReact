38cbc1cc4a3a08aa01e257b106b8475d86865968
manuz7 <bassettonavesmanu@gmail.com>
2023-08-07 12:24:56 -0300

    Atualização

--- Projetos/restaurante/src/pages/Cardapio/index.tsx
+++ Projetos/restaurante/src/pages/Cardapio/index.tsx
@@ -1,7 +1,14 @@
+import styles from './Cardapio.module.scss';
+import logo from '../../assets/logo.svg';
+
+
 export default function Cardapio() {
    return(
     <main>
-        <nav>Restaurante</nav>
+        <nav className={styles.menu}>
+            <img src={logo} alt="logo" />
+        </nav>
     </main>
+   
    );
 }
