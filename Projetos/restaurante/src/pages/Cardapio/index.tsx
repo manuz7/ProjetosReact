199189217fca2891c43c0158b3b9381f203c0adf
manuz7 <bassettonavesmanu@gmail.com>
2023-09-04 12:24:31 -0300

    atualizacao

--- Projetos/restaurante/src/pages/Cardapio/index.tsx
+++ Projetos/restaurante/src/pages/Cardapio/index.tsx
@@ -1,8 +1,11 @@
 import styles from './Cardapio.module.scss';
 import logo from 'assets/logo.svg';
+import Buscador from './Buscador';
+import { useState } from 'react';
 
 
 export default function Cardapio() {
+    const[busca, setBusca] = useState("");
    return(
     <main>
         <nav className={styles.menu}>
@@ -13,6 +16,10 @@ export default function Cardapio() {
                   A casa do código é massa!!
             </div>
         </header>
+        <section className={styles.cardapio}>
+            <h3 className={styles.cardapio__tirulo}>Cardápio</h3>
+            <Buscador busca={busca} setBusca={setBusca} />
+        </section>
     </main>
    
    );
